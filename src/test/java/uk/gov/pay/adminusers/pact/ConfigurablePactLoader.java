cc809656e6bba7d0a7903d7e4ec4495003f7b09a
Alex Bishop <alexbishop1@users.noreply.github.com>
2017-04-05 10:11:06 +0100

    PP-1347 Use a local pact directory rather than a broker
    with @mrlumbu

--- src/test/java/uk/gov/pay/adminusers/pact/ConfigurablePactLoader.java
+++ src/test/java/uk/gov/pay/adminusers/pact/ConfigurablePactLoader.java
@@ -2,6 +2,7 @@ package uk.gov.pay.adminusers.pact;
 
 import au.com.dius.pact.model.Pact;
 import au.com.dius.pact.provider.junit.loader.PactBrokerLoader;
+import au.com.dius.pact.provider.junit.loader.PactFolderLoader;
 import au.com.dius.pact.provider.junit.loader.PactLoader;
 
 import java.io.IOException;
@@ -18,16 +19,21 @@ public class ConfigurablePactLoader implements PactLoader {
 
     @Override
     public List<Pact> load(String providerName) throws IOException {
-        Properties properties = new Properties();
+        if ("local".equals(System.getProperty("pactSource"))) {
+            PactFolderLoader pfl = new PactFolderLoader("pacts");
+            return pfl.load(providerName);
+        } else {
+            Properties properties = new Properties();
 
-        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config/pact.properties"));
-        String host = properties.getProperty("host", "localhost");
-        String port = properties.getProperty("port", "80");
-        String protocol = properties.getProperty("protocol", "https");
-        List<String> tags = Arrays.asList(System.getProperty("pactTags").split(","));
-        PactBrokerLoader pbl = new PactBrokerLoader(host, port, protocol, tags);
+            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config/pact.properties"));
+            String host = properties.getProperty("host", "localhost");
+            String port = properties.getProperty("port", "80");
+            String protocol = properties.getProperty("protocol", "https");
+            List<String> tags = Arrays.asList(System.getProperty("pactTags").split(","));
+            PactBrokerLoader pbl = new PactBrokerLoader(host, port, protocol, tags);
 
-        return pbl.load(providerName);
+            return pbl.load(providerName);
+        }
     }
 
 }
